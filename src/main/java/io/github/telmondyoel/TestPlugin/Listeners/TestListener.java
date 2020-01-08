bc307ee18d2b990dfea4e0a48bba83f78c008efb
telmondyoel <telmondyoel@yahoo.co.uk>
2020-01-09 00:05:03 +0200

    Update TestListener.java
    changed logic

--- src/main/java/io/github/telmondyoel/TestPlugin/Listeners/TestListener.java
+++ src/main/java/io/github/telmondyoel/TestPlugin/Listeners/TestListener.java
@@ -1,19 +1,27 @@
 package io.github.telmondyoel.TestPlugin.Listeners;
 
-import org.bukkit.Bukkit;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Enderman;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+
+import static org.bukkit.World.Environment.THE_END;
+import static org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason.NATURAL;
 
 public class TestListener implements Listener {
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Bukkit.broadcastMessage(event.getPlayer().getName() + " is pretty cool");
-    }
+    public void onEndermanSpawnInEnd(CreatureSpawnEvent event) {
+        LivingEntity livingEntity = event.getEntity();
 
-    @EventHandler
-    public void onBedThing(PlayerBedEnterEvent event) {
-        Bukkit.broadcastMessage("The bed is at: " + event.getBed().getLocation().toString());
+        if (event.getSpawnReason() == NATURAL &&
+                livingEntity.getWorld().getEnvironment().equals(THE_END) &&
+                livingEntity instanceof Enderman) {
+
+            livingEntity.getWorld().spawn(event.getLocation(), Creeper.class);
+            livingEntity.remove();
+        }
+
     }
 }
